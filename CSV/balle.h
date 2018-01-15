eb106a3379beeb2157ff551781a4996a735cf95a
Grim <monnot@et.esiea.fr>
2018-01-15 10:06:31 +0100

    Ajout struct
    balle.h

--- CSV/balle.h
+++ CSV/balle.h
@@ -9,6 +9,16 @@
 #ifndef balle_h
 #define balle_h
 
+typedef struct Balle_decl
+{
+
+  float masse; //Kilo
+  float coeffriction;
+  Vecteur position; //m
+  Vecteur vitesse; // m/s
+  Vecteur acceleration; // m/s2
+
+} Balle;
 
 
 #endif /* balle_h */
