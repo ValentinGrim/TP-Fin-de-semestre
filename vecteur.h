ed578aeb275feb6b323bf4311641bf8d19ae8cb1
Soulthefreeze <35447385+Soulthefreeze@users.noreply.github.com>
2018-01-15 09:55:13 +0100

    Update vecteur.h
    Ajout structure

--- vecteur.h
+++ vecteur.h
@@ -8,7 +8,17 @@
 
 #ifndef vecteur_h
 #define vecteur_h
+typedef struct Vecteur_decl
+{
+	float x;
+	float y;
+} Vecteur;
 
+Vecteur creerVect(float x,float y);
+
+Vecteur multScalVEct(float s, Vecteur v);
+
+Vecteur addVect(Vecteur v1, Vecteur v2);
 
 
 #endif /* vecteur_h */
