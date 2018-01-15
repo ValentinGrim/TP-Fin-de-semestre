19715c02505860d335bcf6a1d39bcce0bc39d412
Soulthefreeze <35447385+Soulthefreeze@users.noreply.github.com>
2018-01-15 10:36:30 +0100

    Update vecteur.c

--- CSV/vecteur.c
+++ CSV/vecteur.c
@@ -9,8 +9,29 @@
 #include <stdio.h>
 #include "vecteur.h"
 
-Vecteur creerVect(float x,float y)
+Vecteur creerVect(float x,float y) // Retourne une structure Vecteur initialisee a (x,y)
 {
+	Vecteur v;
+	v.x=x;
+	v.y=y;
 
+	return v;
+}
 
+Vecteur multScalVect(float s,Vecteur v) // Multiplie un scalaire avec un vecteur et retourne le resultat
+{
+	Vecteur v1;
+	v1.x=s*v.x;
+	v1.y=s*v.y;
+	
+	return v1;
+}
+
+Vecteur addVect(Vecteur v1,Vecteur v2) // Additionne 2 vecteurs et retourne le resultat
+{
+	Vecteur v3;
+	v3.x=v1.x+v2.x;
+	v3.y=v1.y+v2.y;
+	
+	return v3;
 }
