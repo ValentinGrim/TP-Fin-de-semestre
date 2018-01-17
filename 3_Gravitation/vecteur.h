cd0b2c61fb085d8c232830fb3a14a10fd90606a3
Soulthefreeze <35447385+Soulthefreeze@users.noreply.github.com>
2018-01-17 17:49:12 +0100

    Final

--- 3_Gravitation/vecteur.h
+++ 3_Gravitation/vecteur.h
@@ -9,22 +9,19 @@
 #ifndef vecteur_h
 #define vecteur_h
 
-typedef struct Vecteur_decl
+typedef struct Vecteur_decl			// Définition de la structure Vecteur_decl
 {
 	float x;
 	float y;
 } Vecteur;
 
-Vecteur creerVect(float x,float y);
+Vecteur creerVect(float x,float y);		// Prototype de la fonction creerVect
+Vecteur multScalVect(float s, Vecteur v);	// Prototype de la fonction multScalVect
+Vecteur addVect(Vecteur v1, Vecteur v2);	// Prototype de la fonction addvect
 
-Vecteur multScalVect(float s, Vecteur v);
 
-Vecteur addVect(Vecteur v1, Vecteur v2);
+Vecteur normaliseVect (Vecteur v);		// Prototype de la fonction normaliseVect
+float normVect(Vecteur v);			// Prototype de la fonction normVect
+Vecteur subVect(Vecteur v1, Vecteur v2);	// Prototype de la fonction subVect
 
-Vecteur normaliseVect (Vecteur v);
-
-float normVect(Vecteur v);
-
-Vecteur subVect(Vecteur v1, Vecteur v2);
-
-#endif /* vecteur_h */
+#endif 
