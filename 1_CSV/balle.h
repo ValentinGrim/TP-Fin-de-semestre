03cc639ac6d5fcf685734f86c6e98f9a16cda1a5
Soulthefreeze <35447385+Soulthefreeze@users.noreply.github.com>
2018-01-17 17:01:53 +0100

    Final

--- 1_CSV/balle.h
+++ 1_CSV/balle.h
@@ -14,20 +14,16 @@
 typedef struct Balle_decl
 {
 
-  float masse; //Kilo
+  float masse;              //Kilo
   float coeffriction;
-  Vecteur position; //m
-  Vecteur vitesse; // m/s
-  Vecteur acceleration; // m/s2
+  Vecteur position;         //m
+  Vecteur vitesse;          // m/s
+  Vecteur acceleration;     // m/s2
 
 } Balle;
 
 Balle chargerBalle(char * argc);
 
+int majPosition(Balle * Balle ,float dt);     // Prototype de la fonction majPosition
 
-int majPosition(Balle * Balle ,float dt);     // Met a jour la position de la balle en appliquant le PFD
-                                              //retourne -1 si balle est NULL (securite)
-
-
-
-#endif /* balle_h */
+#endif 
