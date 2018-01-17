51ea5f528dd48d5afbe23cdfefd1b0b813131037
Soulthefreeze <35447385+Soulthefreeze@users.noreply.github.com>
2018-01-17 18:15:38 +0100

    Final

--- 4_Ressorts/balle.h
+++ 4_Ressorts/balle.h
@@ -12,24 +12,21 @@
 #include "vecteur.h"
 
 
-typedef struct Balle_decl
+typedef struct Balle_decl             // Définition de la structure Balle_decl
 {
 
-  float masse; //Kilo
+  float masse;              //Kilo
   float coeffriction;
-  Vecteur position; //m
-  Vecteur vitesse; // m/s
-  Vecteur acceleration; // m/s2
+  Vecteur position;         //m
+  Vecteur vitesse;          // m/s
+  Vecteur acceleration;     // m/s2
   struct Balle_decl * ballePrecedente;
   struct Balle_decl * balleSuivante;
+
 } Balle;
 
-Balle chargerBalle(char * argc);
+Balle chargerBalle(char * argc);        // Prototype de la fonction chargerBalle
 
-
-int majPosition(Balle * Balle ,float dt);     // Met a jour la position de la balle en appliquant le PFD
-                                              //retourne -1 si balle est NULL (securite)
-
-
+int majPosition(Balle * Balle ,float dt); // Prototype de la fonction majPosition
 
 #endif /* balle_h */
