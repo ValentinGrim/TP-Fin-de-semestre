ef25b89d4eeb165109b1f14c5ca3e4fe1bd8dffa
Soulthefreeze <35447385+Soulthefreeze@users.noreply.github.com>
2018-01-17 17:05:28 +0100

    Final

--- 1_CSV/vecteur.c
+++ 1_CSV/vecteur.c
@@ -20,7 +20,7 @@ Vecteur creerVect(float x,float y)    // Retourne une structure Vecteur initiali
 	return v;
 }
 
-Vecteur multScalVect(float s,Vecteur v) // Multiplie un scalaire avec un vecteur et retourne le resultat
+Vecteur multScalVect(float s,Vecteur v)   // Multiplie un scalaire avec un vecteur et retourne le resultat
 {
 	Vecteur v1;
 	v1.x=s*v.x;
@@ -29,7 +29,7 @@ Vecteur multScalVect(float s,Vecteur v) // Multiplie un scalaire avec un vecteur
 	return v1;
 }
 
-Vecteur addVect(Vecteur v1,Vecteur v2) // Additionne 2 vecteurs et retourne le resultat
+Vecteur addVect(Vecteur v1,Vecteur v2)   // Additionne 2 vecteurs et retourne le resultat
 {
 	Vecteur v3;
 	v3.x=v1.x+v2.x;
