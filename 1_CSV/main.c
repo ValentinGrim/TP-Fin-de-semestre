0e67e060f12b7d6865102675b3ba02292106dd0c
Soulthefreeze <35447385+Soulthefreeze@users.noreply.github.com>
2018-01-18 09:13:11 +0100

    Final

--- 1_CSV/main.c
+++ 1_CSV/main.c
@@ -14,7 +14,7 @@
 int main(int agrc, char ** argv)
 {
 
-    if (agrc < 3) //test des arguments entrée de le main quitter s'il n'y en a pas assez.
+    if (agrc < 3) // Test du nombre d'arguments minimum d'entrée en console sinon quitte si il n'y en a pas assez.
     {
 
       printf("Il manque un ou plusieurs arguments.");
@@ -22,19 +22,19 @@ int main(int agrc, char ** argv)
 
     }
 
-    Balle Balle_v = chargerBalle(argv[1]); //Déclaration de la balle et chargment des parametre du fichier texte.
+    Balle Balle_v = chargerBalle(argv[1]); // Déclaration de la balle et chargement des paramètres du fichier texte.
 
-    FILE *pFichier = NULL; //Déclaration pointeur fichier
-    pFichier = fopen(argv[2], "w"); //ouverture csv et assignation du pointeur fichier
+    FILE *pFichier = NULL; // Déclaration du pointeur sur fichier
+    pFichier = fopen(argv[2], "w"); // Ouverture du fichier texte et assignation du pointeur sur fichier
 
-    float t; //Variable de temps
+    float t; // Variable du temps
 
     for(t = 0.0; t <= 3; t += 0.01) // Boucle de caclule de nouvelle position de la balle
     {
 
-        fprintf(pFichier, "%f;%f;%f\n", t, Balle_v.position.x, Balle_v.position.y); //Ecriture de la position de la balle et du temps dans le csv
-        int secu = majPosition(&Balle_v, 0.01); // Mise a jours avec les nouvelle coordonées, vitese ect... +  renvoi -1 a secu si Balle NULL
-        if(secu == -1) return EXIT_FAILURE; //Sécurité
+        fprintf(pFichier, "%f;%f;%f\n", t, Balle_v.position.x, Balle_v.position.y); // Ecriture de la position de la balle et du temps dans le csv
+        int secu = majPosition(&Balle_v, 0.01); // Mise à jour avec les nouvelles coordonées, vitesses, ect... +  renvoi -1 à secu si la Balle est NULL
+        if(secu == -1) return EXIT_FAILURE;     //Sécurité
 
     }
 
