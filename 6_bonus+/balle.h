f2c80c96089a8d2c06f235a82bd4c0d3badfd596
Soulthefreeze <35447385+Soulthefreeze@users.noreply.github.com>
2018-01-18 19:41:40 +0100

    Voila y'a mon bonus avec

---
+++ 6_bonus+/balle.h
@@ -0,0 +1,32 @@
+//
+//  balle.h
+//  CSV
+//
+//  Created by Valentin Monnot / Josselin Pichereau on 15/01/2018.
+//  Evalution TP Final
+//
+
+#ifndef balle_h
+#define balle_h
+
+#include "vecteur.h"
+
+
+typedef struct Balle_decl             // Définition de la structure Balle_decl
+{
+
+  float masse;              //Kilo
+  float coeffriction;
+  Vecteur position;         //m
+  Vecteur vitesse;          // m/s
+  Vecteur acceleration;     // m/s2
+  struct Balle_decl * ballePrecedente;
+  struct Balle_decl * balleSuivante;
+
+} Balle;
+
+Balle chargerBalle(char * argc);        // Prototype de la fonction chargerBalle
+
+int majPosition(Balle * Balle ,float dt); // Prototype de la fonction majPosition
+
+#endif /* balle_h */
