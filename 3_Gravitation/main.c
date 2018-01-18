846df398f500e319dfbd1b4941034e127d85e1c0
Soulthefreeze <35447385+Soulthefreeze@users.noreply.github.com>
2018-01-18 09:03:35 +0100

    Final

--- 3_Gravitation/main.c
+++ 3_Gravitation/main.c
@@ -7,19 +7,18 @@
 #include "decl.h"
 #include "fpstimer.h"
 
-AttracteurList gAttractList; //Variable global du nombre et de la position des attracteurs
+AttracteurList gAttractList;    // Variable global du nombre et de la position des attracteurs
 
-    // fonction appell�e lorsque l'on appuie avec le bouton gauche de la souris
-void clicSouris(float x, float y)
+    
+void clicSouris(float x, float y)       // fonction appellée lorsque l'on appuie avec le bouton gauche de la souris
 {
     // NB : une variable locale declaree static ne perd pas sa valeur entre deux appels a 	la fonction
-    // son initialisation est effectu� au tout premier appel de la fonction
+    // son initialisation est effectué au tout premier appel de la fonction
     // on s'en sert ici pour savoir quel attracteur on est en train de positioner
     static int attracteurCurrent = 0 ;
 
-    // TODO gerer les position de la liste des attracteurs ici
-    gAttractList.utiliseMoi[attracteurCurrent] = 1; // Mettre la variable utiliseMoi a 1 si un attracteur a été posé (initialement a 0)
-    gAttractList.positionAttracteur[attracteurCurrent] = creerVect(x,y); //Créé le vecteur positionAttracteuravec les valeurs x et y du clic souris
+    gAttractList.utiliseMoi[attracteurCurrent] = 1; // Mettre la variable utiliseMoi à 1 si un attracteur a été posé (initialement a 0)
+    gAttractList.positionAttracteur[attracteurCurrent] = creerVect(x,y); // Créé le vecteur positionAttracteur avec les valeurs x et y du clic souris
 
     sdl_setAttracteurPosition(attracteurCurrent,x,y);
     attracteurCurrent ++;
@@ -30,15 +29,15 @@ void clicSouris(float x, float y)
 int main ( int argc, char** argv )
 {
 
-    if (agrc < 2) //test des arguments entrée de le main quitter s'il n'y en a pas assez.
+    if (agrc < 2) // Test du nombre d'arguments d'entrée en console sionon quitte si il n'y en a pas assez.
     {
 
       printf("Il manque un ou plusieurs arguments.");
       return EXIT_FAILURE; // On quitte le programme
 
     }
-    float dt; // Variable delta donner le temps entre deux frame
-    Balle Balle_v = chargerBalle(argv[1]); //Déclaration de la balle et chargment des parametre du fichier texte.
+    float dt; // Déclaration de la variable delta
+    Balle Balle_v = chargerBalle(argv[1]); // Déclaration de la balle et chargement des paramètres du fichier texte.
 
     if(!sdl_startup())
         return -1;
@@ -49,42 +48,41 @@ int main ( int argc, char** argv )
     {
         fpsStep();
 
-        // ----------------- TODO: remplacer par votre code --------------
-        dt=fpsGetDeltaTime(); // Assigner le temsp entre deux frames a la variable dt
-        majPosition(&Balle_v, &gAttractList, dt); //Met a jours la position de la balle en fonction de dt et de la liste des attracteurs
+        
+        dt=fpsGetDeltaTime(); // Assigne le temps entre deux frames à la variable dt
+        majPosition(&Balle_v, &gAttractList, dt); // Met à jour la position de la balle en fonction de dt et de la liste des attracteurs
 
-        if(Balle_v.position.x < BALL_RADIUS) //Condition si balle touche le bord gauche
+        if(Balle_v.position.x < BALL_RADIUS) // Condition si balle touche le bord gauche
         {
 
-            Balle_v.vitesse.x = (-Balle_v.vitesse.x) * 0.9; //inversement de la vitesse pour faire partir la balle dans l'autre sens plus reduction de la vitesse (perte d'energie)
-            Balle_v.position.x = BALL_RADIUS; //Mise a jours de la position de la balle pour ne pas qu'elle sorte de l'écran
+            Balle_v.vitesse.x = (-Balle_v.vitesse.x) * 0.9; // Inversement de la vitesse pour faire partir la balle dans l'autre sens et reduction de la vitesse (perte d'energie)
+            Balle_v.position.x = BALL_RADIUS;               // Mise à jour de la position de la balle pour qu'elle ne sorte pas de l'écran
 
         }
 
         if(Balle_v.position.x > 1 - BALL_RADIUS) //Condition si balle touche le bord droit
         {
 
-            Balle_v.vitesse.x = (-Balle_v.vitesse.x) * 0.9; //inversement de la vitesse pour faire partir la balle dans l'autre sens plus reduction de la vitesse (perte d'energie)
-            Balle_v.position.x = 1 - BALL_RADIUS; //Mise a jours de la position de la balle pour ne pas qu'elle sorte de l'écran
-
+            Balle_v.vitesse.x = (-Balle_v.vitesse.x) * 0.9; // Inversement de la vitesse pour faire partir la balle dans l'autre sens et reduction de la vitesse (perte d'energie)
+            Balle_v.position.x = 1 - BALL_RADIUS;           // Mise à jour de la position de la balle pour qu'elle ne sorte pas de l'écran
         }
 
         if(Balle_v.position.y < BALL_RADIUS) //Condition si balle touche le bord haut
         {
 
-            Balle_v.vitesse.y = (-Balle_v.vitesse.y) * 0.9; //inversement de la vitesse pour faire partir la balle dans l'autre sens plus reduction de la vitesse (perte d'energie)
-            Balle_v.position.y = BALL_RADIUS; //Mise a jours de la position de la balle pour ne pas qu'elle sorte de l'écran
+            Balle_v.vitesse.y = (-Balle_v.vitesse.y) * 0.9; // Inversement de la vitesse pour faire partir la balle dans l'autre sens et reduction de la vitesse (perte d'energie)
+            Balle_v.position.y = BALL_RADIUS;               // Mise à jour de la position de la balle pour qu'elle ne sorte pas de l'écran
 
         }
 
         if(Balle_v.position.y > 1 - BALL_RADIUS) //Condition si balle touche le bord bas
 
-            Balle_v.vitesse.y = (-Balle_v.vitesse.y) * 0.9; //inversement de la vitesse pour faire partir la balle dans l'autre sens plus reduction de la vitesse (perte d'energie)
-            Balle_v.position.y = 1 - BALL_RADIUS; //Mise a jours de la position de la balle pour ne pas qu'elle sorte de l'écran
+            Balle_v.vitesse.y = (-Balle_v.vitesse.y) * 0.9; // Inversement de la vitesse pour faire partir la balle dans l'autre sens et reduction de la vitesse (perte d'energie)
+            Balle_v.position.y = 1 - BALL_RADIUS;           // Mise à jour de la position de la balle pour qu'elle ne sorte pas de l'écran
 
         }
 
-        sdl_setBallPosition(Balle_v.position.x,Balle_v.position.y); //Permet d'afficher la balle avec sa nouvelle position.
+        sdl_setBallPosition(Balle_v.position.x,Balle_v.position.y); // Permet d'afficher la balle avec sa nouvelle position.
 
     } while(sdl_loop());
     // end main loop
