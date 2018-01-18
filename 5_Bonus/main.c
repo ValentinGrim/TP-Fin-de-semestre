634e71f3c319e3333fb9aa405e1ffdb564722caa
Grim <34299375+ValentinGrim@users.noreply.github.com>
2018-01-18 11:54:16 +0100

    JCP

--- 5_Bonus/main.c
+++ 5_Bonus/main.c
@@ -10,20 +10,23 @@
 #include <SDL/SDL.h>
 #include "main.h"
 
+void clicSouris(float x, float y)  //Clique souris de Gravitation
+{
+
+
+}
+
 int main()
 {
-  pBouton bPlay;
-  pBouton bSet;
-  pBouton bExit;
 
   SDL_Surface *window = NULL;
   SDL_Event event;
 
-  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER);
 
   window = SDL_SetVideoMode(256, 512, 32,SDL_HWSURFACE|SDL_DOUBLEBUF);
 
-  if (window == NULL) // Si l'ouverture a échoué, on le note et on arrête
+  if (window == NULL)
   {
 
       printf("Impossible d'ouvrir la fenêtre : %s\n", SDL_GetError());
@@ -32,15 +35,7 @@ int main()
   }
 
   SDL_WM_SetCaption("Menu Bonus", NULL);
-  SDL_FillRect(window, NULL, SDL_MapRGB(window->format,245,80,0);
-  bPlay.x = 128;
-  bPlay.y = 64;
-
-  bSet.x  = 128;
-  bSet.y = 192;
-
-  bExit.x = 128;
-  bExit.y = 448;
+  SDL_FillRect(window, NULL, SDL_MapRGB(window->format,245,80,0));
 
   bPlayImg = SDL_LoadBMP("Button_Play.bmp");
   bSetImg = SDL_LoadBMP("Button_Settings.bmp");
@@ -58,26 +53,12 @@ int main()
         continuer = 0;
         break;
 
-      case SDL_MOUSEBUTTONUP:
-        if(event.button.y > 38 && event.button.y < 90 && event.button.x > 27 && event.button.x < 229)
-        {
-
-        }
-        break;
-
-        case SDL_MOUSEBUTTONUP:
-          if(event.button.y > 166 && event.button.y < 218 && event.button.x > 27 && event.button.x < 229)
-          {
-
-          }
-          break;
-
         case SDL_MOUSEBUTTONUP:
           if(event.button.y > 422 && event.button.y < 474 && event.button.x > 27 && event.button.x < 229)
           {
 
               continuer = 0;
-              
+
           }
           break;
 
